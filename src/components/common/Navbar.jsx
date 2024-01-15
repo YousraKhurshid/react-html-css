bf31ebd70685672585be68a6fd3076e07ae0720c
Yousra Khurshid <YousraKhurshid1@gmail.com>
2024-01-15 23:48:06 +0500

    done

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <a href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
-            <a href="#services">Services</a>
+            <a href="#services">ServicesPage</a>
             <a href="#portfolio">Portfolio</a>
             <a href="#news">News</a>
               </div>
